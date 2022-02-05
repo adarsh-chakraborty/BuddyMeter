44e4432fa99dcd63b3378b69dc1dc232131bb6d0
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-02-05 18:36:39 +0530

    Project Reset: Removed Bootstrap, Installed tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
-import QuizCreation from './screens/QuizCreation';
-import CreateAccount from './screens/CreateAccount';
-import LoginScreen from './screens/LoginScreen';
-
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/create-quiz" element={<QuizCreation />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/login" element={<LoginScreen />} />
-      </Routes>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
