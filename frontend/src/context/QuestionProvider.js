ba37ae03030081fb15c7086c383bd5aa365e5088
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-02-22 21:57:20 +0530

    Basic reducer setup and dummy questions init

--- frontend/src/context/QuestionProvider.js
+++ frontend/src/context/QuestionProvider.js
@@ -1,18 +1,38 @@
 import QuestionContext from './question-context';
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
+import questions from '../questions.json';
+import { FETCH_QUESTIONS } from './constants';
 
-const defaultQuestionState = {
+const defaultState = {
   userQuestions: [],
   currentIndex: 0,
   questions: []
 };
 
 const questionReducer = (state, action) => {
-  return defaultQuestionState;
+  console.log(state, action.type);
+  switch (action.type) {
+    case FETCH_QUESTIONS: {
+      return {
+        ...state,
+        questions: questions
+      };
+    }
+    default:
+      return state;
+  }
 };
 
 const QuestionProvider = (props) => {
-  const [state, setState] = useReducer(questionReducer, defaultQuestionState);
+  const [state, stateActionDispatch] = useReducer(
+    questionReducer,
+    defaultState
+  );
+
+  useEffect(() => {
+    console.log('App Initialized');
+    stateActionDispatch({ type: FETCH_QUESTIONS });
+  }, []);
 
   const questionContext = {
     // add variables and functions here
